46bc78d24ea8394c12321ecb4a65d44747306858
tf63 <tfukuda9031@gmail.com>
2023-04-11 23:30:27 +0900

    abc289

--- contest/abc/abc289/c/abc289_c.cpp
+++ contest/abc/abc289/c/abc289_c.cpp
@@ -169,7 +169,52 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     // ----------------------------------------------------------------
+    int N, M;
+    cin >> N >> M;
 
+    vector<vec> A(M, vec());
+    rep(i, M) {
+        int C;
+        cin >> C;
+        int a;
+        rep(j, C) {
+            cin >> a;
+            A[i].push_back(a);
+        }
+    }
+    ll ans = 0;
+    int n = M;
+    // {0, 1, ..., n-1} の部分集合の全探索
+    for (int bit = 0; bit < (1 << n); ++bit) {
+        /* bit で表される集合の処理を書く */
+
+        /* きちんとできていることを確認してみる */
+        // bit の表す集合を求める
+        vector<int> S;
+        for (int i = 0; i < n; ++i) {
+            if (bit & (1 << i)) {  // i が bit に入るかどうか
+                S.push_back(i);
+            }
+        }
+
+        // bit の表す集合の出力
+        set<int> x;
+        prep(i, N) {
+            x.insert(i);
+        }
+
+        for (int i = 0; i < (int)S.size(); ++i) {
+            for (auto a : A[S[i]]) {
+                x.erase(a);
+            }
+        }
+
+        if (x.empty()) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
     // ----------------------------------------------------------------
     return 0;
 }
